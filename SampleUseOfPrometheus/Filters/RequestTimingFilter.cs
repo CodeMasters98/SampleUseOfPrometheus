04dbf783d263f1becff974f43ba3ce6a85f81300
darvishiparham14@gmail.com <darvishiparham14@gmail.com>
2024-09-10 15:28:09 +0330

    Change On Request

---
+++ SampleUseOfPrometheus/Filters/RequestTimingFilter.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System.Diagnostics;
+
+namespace SampleUseOfPrometheus.Filters;
+
+public class RequestTimingFilter : IActionFilter
+{
+    public void OnActionExecuting(ActionExecutingContext context)
+    {
+        context.HttpContext.Items["Timer"] = Stopwatch.StartNew();
+    }
+    public void OnActionExecuted(ActionExecutedContext context)
+    {
+        var timer = context.HttpContext.Items["Timer"] as Stopwatch;
+        timer.Stop();
+        //Record request duration
+        //var appMetrics = context.HttpContext.RequestServices.GetRequiredService<IAppMetrics>();
+        //appMetrics.Measure.RequestDuration.Measure(timer.Elapsed);
+    }
+}
